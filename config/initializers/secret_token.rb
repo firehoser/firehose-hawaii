72b7bf17baae771b72b063ae5daa7b75a1652df5
Firehose <firehose@dhcp-168-105-81-254.wireless.manoa.hawaii.edu>
2014-01-04 13:30:46 -1000

    building out our firehoseapp landing page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseHawaii::Application.config.secret_key_base = 'b16dcc28b7d5ff2c8c6d0f4a3897e55a39b4b8d7d9a2ba16213a8400be8b59ef5772533f9b1bca8fe8bcecd5523e091a6d4d3b406e15aca62cb4259996e7a8b6'
---
+++ slater.rb
@@ -0,0 +1,7 @@
+a = 12
+while a < 20 do
+	puts "Still doing stuff #{a}..."
+	sleep(1)
+	a = a + 1
+	
+end
